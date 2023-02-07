0c90fa1ce3b72ba794389d262b08d59b1b203829
Rohan <36108401+rohanap@users.noreply.github.com>
2023-02-07 17:23:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,49 +1,52 @@
-
 import streamlit
-import pandas 
+import pandas
 import requests
 import snowflake.connector
 from urllib.error import URLError
 
 
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
+my_fruit_list = pandas.read_csv(
+    "https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt"
+)
+my_fruit_list = my_fruit_list.set_index("Fruit")
 
-streamlit.title('My Parent new healthy dinner')
-streamlit.header('Breakfast menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text(' 🥗Kale, Spinach & Rocket Smoothie')
-streamlit.text(' 🐔 Hard-Boiled Free-Range Egg')
+streamlit.title("My Parent new healthy dinner")
+streamlit.header("Breakfast menu")
+streamlit.text("Omega 3 & Blueberry Oatmeal")
+streamlit.text(" 🥗Kale, Spinach & Rocket Smoothie")
+streamlit.text(" 🐔 Hard-Boiled Free-Range Egg")
 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.header("🍌🥭 Build Your Own Fruit Smoothie 🥝🍇")
 
-#Let's put a pick list here so they can pick the fruit they want to include
-fruits_selected = streamlit.multiselect('Pick some fruit:',list(my_fruit_list.index),['Avocado','Strawberries'])
+# Let's put a pick list here so they can pick the fruit they want to include
+fruits_selected = streamlit.multiselect(
+    "Pick some fruit:", list(my_fruit_list.index), ["Avocado", "Strawberries"]
+)
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-#streamlit.dataframe(my_fruit_list)
+# streamlit.dataframe(my_fruit_list)
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 try:
-   fruit_choice = streamlit.text_input('What fruit would you like information about?')
-   if not fruit_choice:
-        #streamlit.write('The user entered ', fruit_choice)
+    fruit_choice = streamlit.text_input("What fruit would you like information about?")
+    if not fruit_choice:
+        # streamlit.write('The user entered ', fruit_choice)
         streamlit.error("Please select a fruit to get information:")
-   else:
-       fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
-       fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-       streamlit.dataframe(fruityvice_normalized)
-       
- except URLError as e:
-       streamlit.error()
-#streamlit.text(fruityvice_response.json()) # write data on screen
+    else:
+        fruityvice_response = requests.get(
+            "https://fruityvice.com/api/fruit/" + fruit_choice
+        )
+        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+        streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+    streamlit.error()
+# streamlit.text(fruityvice_response.json()) # write data on screen
 
 
-
-# write your own comment -what does the next line do? 
-#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write your own comment -what does the next line do?
+# fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
-#streamlit.dataframe(fruityvice_normalized)
+# streamlit.dataframe(fruityvice_normalized)
 
 streamlit.stop()
 
@@ -55,9 +58,11 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.text("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
-#streamlit.text(my_data_row)
+# streamlit.text(my_data_row)
 
-add_mmy_fruit = streamlit.text_input('What fruit would you like information about?','jackfruit')
-streamlit.write('The user entered ', add_mmy_fruit)
+add_mmy_fruit = streamlit.text_input(
+    "What fruit would you like information about?", "jackfruit"
+)
+streamlit.write("The user entered ", add_mmy_fruit)
 
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
